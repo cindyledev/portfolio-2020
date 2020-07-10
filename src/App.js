6fc24f8d97bbdce0a47232f55c4a8523783cd9ab
Cindy Le <cindyledev@gmail.com>
2020-07-10 16:45:02 -0400

    moved Portfolio into separate component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import React from "react";
 import clsx from "clsx";
 import {
   AppBar,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
   Container,
   CssBaseline,
   Drawer,
   Divider,
-  Grid,
   IconButton,
   Link,
   List,
@@ -33,6 +27,7 @@ import SchoolIcon from "@material-ui/icons/School";
 import StarIcon from "@material-ui/icons/Star";
 
 import About from "./About";
+import Portfolio from "./Portfolio";
 
 const drawerWidth = 240;
 
@@ -92,24 +87,11 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
   footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
 }));
-
-const cards = [1, 2, 3, 4, 5, 6];
 
 export default function App() {
   const classes = useStyles();
@@ -154,40 +136,9 @@ export default function App() {
         })}
       >
         <div className={classes.drawerHeader} />
-        {/* Hero unit */}
         <About />
         <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          <Portfolio />
         </Container>
       </main>
       <Drawer
---
+++ src/Portfolio.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6];
+
+export default function Portfolio() {
+  const classes = useStyles();
+
+  return (
+    <Grid container spacing={4}>
+      {cards.map((card) => (
+        <Grid item key={card} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cardMedia}
+              image="https://source.unsplash.com/random"
+              title="Image title"
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Heading
+              </Typography>
+              <Typography>
+                This is a media card. You can use this section to describe the
+                content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary">
+                View
+              </Button>
+              <Button size="small" color="primary">
+                Edit
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
